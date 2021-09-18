d81b532e091609e4ebfad16bc80ab78066fc82d7
sentientyougert <nicholas.ross.lane@gmail.com>
2021-09-18 13:04:21 +0800

    Table Completed
    Working on adding color conditioning

---
+++ public_html/storeData.js
@@ -0,0 +1,16 @@
+const storeData = [
+    { city: "Geelong", postcode: 3220, manager: "Steve Smith", sales: 1256 },
+    { city: "Bayswater", postcode: 3153, manager: "Alice Peeters", sales: 4562 },
+    { city: "Shellharbour", postcode: 2529,  manager: "Brett Jones", sales: 6643 },
+    { city: "Upper Burnie", postcode: 7320,  manager: "Cathy Cail", sales: 1233 },
+    { city: "Woodville", postcode: 5011,  manager: "Eric Pool", sales: 2453 },
+    { city: "Macquarie", postcode: 2614, manager: "Lisa Voom", sales: 2885 },
+    { city: "Yulara", postcode: 0872, manager: "Perrie Roth", sales: 5662 },
+    { city: "Opalton", postcode: 4735, manager: "Di Waters", sales: 89 },
+    { city: "Muntham", postcode: 3315, manager: "Helen Hunt", sales: 2345 },
+    { city: "Brooklands", postcode: 4615,  manager: "Jules Vern", sales: 3301 },
+    { city: "Caiguna", postcode: 6443, manager: "Nate King", sales: 678 },
+    { city: "Beeliar", postcode: 6164, manager: "Wilma Young", sales: 4856 }
+ ];
+
+
